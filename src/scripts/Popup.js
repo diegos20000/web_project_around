5b27beaa59f25cbe3105763a1ce4ae94900ba88e
Diego-Rodrigo-Cano <dsuccess2020@gmail.com>
2024-09-28 13:44:46 -0500

    sprint 11 webpack

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,21 +1,3 @@
-class Section {
-  constructor({ items, renderer }, containerSelector) {
-    this._items = items;
-    this._renderer = renderer;
-    this._container = document.querySelector(containerSelector);
-  }
-
-  renderItems() {
-    this._items.forEach((item) => {
-      this._renderer(item);
-    });
-  }
-
-  addItem(element) {
-    this._container.append(element);
-  }
-}
-
 export default class Popup {
   constructor(popupSelector) {
     this.popup = document.querySelector(popupSelector);
@@ -48,62 +30,6 @@ export default class Popup {
       if (event.target === this.popup) {
         this.close();
       }
-    });
-  }
-}
-
-class UserInfo {
-  constructor({ nameSelector, jobSelector }) {
-    this._nameElement = document.querySelector(nameSelector);
-    this._jobSelector = document.querySelector(jobSelector);
-  }
-
-  getUserInfo() {
-    return {
-      name: this._nameElement.textContent,
-      job: this._jobSelector.textContent,
-    };
-  }
-
-  setUserInfo({ name, job }) {
-    this._nameElement.textContent = name;
-    this._jobElement.textContent = job;
-  }
-}
-
-class Card {
-  constructor(data, templateSelector, handleCardClick) {
-    this._name = data.name;
-    this._link = data.link;
-    this._templateSelector = templateSelector;
-    this._handleCardClick = handleCardClick;
-  }
-
-  _getTemplate() {
-    const cardElement = document
-      .querySelector(this._templateSelector)
-      .content.querySelector(".element")
-      .cloneNode(true);
-    return cardElement;
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    this._cardImage = this._element.querySelector(".element__img");
-    this._cardTitle = this._element.querySelector(".element__text");
-
-    this._cardImage.src = this._link;
-    this._cardImage.alt = this._name;
-    this._cardTitle.textContent = this._name;
-
-    this._setEventListeners();
-
-    return this._element;
-  }
-
-  _setEventListeners() {
-    this._cardImage.addEventListener("click", () => {
-      this._handleCardClick(this._name, this._link);
     });
   }
 }
